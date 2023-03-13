da15d46d6b7e4f77c13f6b7af22930b4f7414860
E. Tramontana <73878438+e-tramontana@users.noreply.github.com>
2023-03-13 21:18:21 +0100

    Update MyTestSupport.java

--- MyTestSupport.java
+++ MyTestSupport.java
@@ -1,3 +1,8 @@
+/** 
+ * Classe di utilità che colora l'output dei test. I nomi dei metodi sono 
+ * conformi alla libreria JUnit per acquisire familiarità con questa libreria 
+ * e per facilitare la migrazione. 
+ */
 public class MyTestSupport {
     private static final String RESET = "\33[0m";
     private static final String GREEN = "\33[1;32m";
