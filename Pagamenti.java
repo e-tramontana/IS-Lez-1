a5b658d558b9f58e69e438630210180ddc02f2ca
E. Tramontana <73878438+e-tramontana@users.noreply.github.com>
2023-03-16 10:48:14 +0100

    Update Pagamenti.java

--- Pagamenti.java
+++ Pagamenti.java
@@ -14,7 +14,7 @@ import java.util.List;
 public class Pagamenti {
     private List<String> importiLetti = new ArrayList<>();
     private List<Float> valori = new ArrayList<>();
-    private float totale:
+    private float totale;
     private float massimo;
 
     /**
